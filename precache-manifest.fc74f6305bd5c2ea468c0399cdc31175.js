0954a0c348bcc8a1711841b43eaedd65f73145a5
ram_virtue <ramprashanth@siamcomputing.com>
2019-05-27 11:52:52 +0530

    Updates

---
+++ precache-manifest.fc74f6305bd5c2ea468c0399cdc31175.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0b646832eb5bd0304daae062618382",
+    "url": "/react-crud/index.html"
+  },
+  {
+    "revision": "26685429cd41fea31102",
+    "url": "/react-crud/static/css/main.578adf68.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-crud/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "26685429cd41fea31102",
+    "url": "/react-crud/static/js/main.7c8e0c8d.chunk.js"
+  },
+  {
+    "revision": "2f3d0c7cc7993cab9d36",
+    "url": "/react-crud/static/js/runtime~main.73d447a4.js"
+  }
+]);
